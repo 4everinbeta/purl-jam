96fc6f4cba194df52bde9b4306a2262bba4e7d32
4everinbeta <4everinbeta@gmail.com>
2026-01-12 15:30:47 -0700

    Fix Oscar URL wiring

--- purljam/urls.py
+++ purljam/urls.py
@@ -1,5 +1,7 @@
+from django.apps import apps
 from django.urls import path
-from oscar.app import application
+
+application = apps.get_app_config("oscar")
 
 urlpatterns = [
     path("", application.urls),
