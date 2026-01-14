e79b9691d60b970eb9b62563475abc3b3a55f30a
4everinbeta <4everinbeta@gmail.com>
2026-01-14 09:33:05 -0700

    Add missing Oscar BasketMiddleware to fix 500 error

--- purljam/settings.py
+++ purljam/settings.py
@@ -26,6 +26,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "oscar.apps.basket.middleware.BasketMiddleware",
 ]
 
 ROOT_URLCONF = "purljam.urls"
