dbece0795227f5c63d41f3b6e820cc00f90cefdd
4everinbeta <4everinbeta@gmail.com>
2026-01-14 14:41:52 -0700

    Add /health endpoint for deployment verification

--- purljam/urls.py
+++ purljam/urls.py
@@ -1,6 +1,8 @@
 from django.apps import apps
 from django.urls import include, path
+from .views import health
 
 urlpatterns = [
+    path("health", health),
     path("", include(apps.get_app_config("oscar").urls[0])),
 ]
