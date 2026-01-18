707f611ea7beb0d89fc420fa0cae738f60ed28a5
4everinbeta <4everinbeta@gmail.com>
2026-01-18 12:04:30 -0700

    Update favicon and enable media serving for shop images

--- purljam/urls.py
+++ purljam/urls.py
@@ -1,6 +1,9 @@
 from django.apps import apps
 from django.contrib import admin
-from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path, re_path
+from django.views.static import serve
 from .views import health, homepage
 
 urlpatterns = [
@@ -9,4 +12,10 @@ urlpatterns = [
     path("", homepage, name="home"),
     path("admin/", admin.site.urls),
     path("shop/", include(apps.get_app_config("oscar").urls[0])),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Force media serving in production (since we are using local storage for this demo)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
